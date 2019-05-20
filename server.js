8668793e243bf745a8d78ef4008189b47c91f558
yushare <582718643@qq.com>
2019-05-20 19:26:43 +0800

    update

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+// 借助 Express 在启动项目
+
 var express = require('express');
 var app = express();
 
 //指定启动服务器到哪个文件夹，如 `build/`
 app.use(express.static('./build'));
 
-//监听端口为3001
-app.listen(3001, function () {
-  console.log(' app listening at http://localhost:3001');
+//监听端口为 3002
+app.listen(3002, function () {
+  console.log(' app listening at http://localhost:3002');
 });
