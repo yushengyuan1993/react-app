b65cf3b99c6835b38be2f35fa237dab3b2e8605e
yushare <582718643@qq.com>
2019-05-26 16:28:34 +0800

    update

--- src/index.js
+++ src/index.js
@@ -6,18 +6,18 @@ import App from './containers/App.js';
 // import routes from './router.js';
 import * as serviceWorker from './serviceWorker';
 
-// const MOUNT_NODE = document.getElementById('root');
+const MOUNT_NODE = document.getElementById('root');
 
-// let render = () => {
-//   ReactDOM.render(
-//     <App routes={routes}/>,
-//     MOUNT_NODE
-//   )
-// }
+let render = () => {
+  ReactDOM.render(
+    <App />,
+    MOUNT_NODE
+  )
+}
 
-// render();
+render();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
