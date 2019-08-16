d86d267a192b4b3fb730059531f6840f3abc46cf
yushare <582718643@qq.com>
2019-08-16 11:52:44 +0800

    update

---
+++ build/precache-manifest.39b7377f5efca7cd9a5d4c432fa2d166.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "068554d09fe848a38c8a4616b96c6794",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "adaa8b73b2510d7fc6b1",
+    "url": "/react-app/static/css/2.4100bda8.chunk.css"
+  },
+  {
+    "revision": "5ef41761f9332965c5e9",
+    "url": "/react-app/static/css/main.4114f8b8.chunk.css"
+  },
+  {
+    "revision": "adaa8b73b2510d7fc6b1",
+    "url": "/react-app/static/js/2.d6a45a10.chunk.js"
+  },
+  {
+    "revision": "5ef41761f9332965c5e9",
+    "url": "/react-app/static/js/main.8aa38f50.chunk.js"
+  },
+  {
+    "revision": "9ed022644c6f38b380ac",
+    "url": "/react-app/static/js/runtime~main.cbc86c42.js"
+  }
+]);
