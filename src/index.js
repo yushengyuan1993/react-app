4aa0d98dfa94bc917edc80ffd55751c6e5ff093b
yushare <582718643@qq.com>
2020-06-11 19:40:16 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 // import { Provider } from 'react-redux';
 
 import './config/base.config';
 
 import * as serviceWorker from './serviceWorker';
+
 import routers from './routes/router';
 
 import './assets/css/init.scss';
@@ -20,10 +21,11 @@ ReactDOM.render(
   <BrowserRouter>
     <Switch>
       {route}
+      <Redirect from='/' to='/product' />
     </Switch>
   </BrowserRouter>
   ,document.getElementById('root')
-)
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
