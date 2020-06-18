d8dad54b8a58868946c354104cdb2442b2b3a273
yushare <582718643@qq.com>
2020-06-18 19:19:12 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
-class App extends React.Component {
-  render () {
+import routers from './routes/router';
+
+const route = routers.map((item, i) => <Route path={item.path} component={item.component} key={i} />);
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() { 
     return (
-      <div className="content">
-        hello.
-      </div>
+      <Router>
+        <Switch>
+          {route}
+          <Redirect from='/' to='/product' />
+        </Switch>
+      </Router>
     );
   }
 }
-
+ 
 export default App;
